6ae571c4977de720dc01155c5c50135659aee925
Alex Camacho <alex.c@WalkMe.local>
2018-12-03 21:43:45 -0800

    instantiated album component

---
+++ src/components/AlbumComponent/AlbumComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './AlbumComponent.css';
+
+class AlbumComponent extends Component {
+    render() {
+        return (
+            <div className="child-component"></div>
+        );
+    }
+}
+
+export default AlbumComponent;
