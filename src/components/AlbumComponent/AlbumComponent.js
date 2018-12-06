81479aaf23760bac3db61eb2af904414b0043a8b
Alex Camacho <alex.c@WalkMe.local>
2018-12-05 22:24:56 -0800

    made card for each album, gave styling to hover over each one, set up place for API to feed in album array

--- src/components/AlbumComponent/AlbumComponent.js
+++ src/components/AlbumComponent/AlbumComponent.js
@@ -4,7 +4,13 @@ import './AlbumComponent.css';
 class AlbumComponent extends Component {
     render() {
         return (
-            <div className="child-component"></div>
+            <div className="outer-album-card">
+                <img className="album-card-image" src={this.props.imgsrc} alt={this.props.title} />
+                <div className="album-card-text">
+                    <p className="album-card-title">{this.props.title}</p>
+                    <p className="album-card-artist">{this.props.artist}</p>
+                </div>
+            </div>
         );
     }
 }
