16303423964d4bf0655354cdfa96b076131d3767
Alex Camacho <alex.c@WalkMe.local>
2018-12-21 23:04:28 -0500

    removed placeholder album content since the spotify API is hooked up now

--- src/components/AlbumContainerComponent/AlbumContainerComponent.js
+++ src/components/AlbumContainerComponent/AlbumContainerComponent.js
@@ -4,19 +4,6 @@ import AlbumComponent from '../AlbumComponent/AlbumComponent.js';
 
 class AlbumContainerComponent extends Component {
     render() {
-        var albumList = [
-            {
-                imgsrc: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5708/5708825_sa.jpg",
-                title: "Dark Side of the Moon",
-                artist: "Pink Floyd"
-            },
-            {
-                imgsrc: "https://www.fye.com/dw/image/v2/BBNF_PRD/on/demandware.static/-/Sites-fye-master/default/dw008227f5/aec/atl/atl535/aec.atl535339.2_0.jpg?sw=584",
-                title: "Physical Graffiti",
-                artist: "Led Zeppelin"
-            }
-        ];
-
         return (
             <div className="album-container-div">
                 {this.props.results.map(result => (
