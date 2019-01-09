07583d14964befc8bfe718531a7299380b71439a
acamacho88 <billyshears06@gmail.com>
2019-01-09 06:00:33 +0000

    added basic play button with CSS

---
+++ src/components/PlayButtonComponent/PlayButtonComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './PlayButtonComponent.css';
+
+class PlayButtonComponent extends Component {
+    render() {
+        return (
+          <div className="play-button">
+          </div>
+        );
+    }
+}
+
+export default PlayButtonComponent;
