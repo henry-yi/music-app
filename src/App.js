486f69afcfde59597437873c80cf182e83bd80b3
acamacho88 <billyshears06@gmail.com>
2019-01-15 05:18:29 +0000

    added page folder for playing the files, and consolidated their components into this folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AlbumContainerComponent from './components/AlbumContainerComponent/AlbumContainerComponent.js';
-import PlayButtonComponent from './components/PlayButtonComponent/PlayButtonComponent.js';
-import SongImageComponent from './components/SongImageComponent/SongImageComponent.js';
-import LyricsComponent from './components/LyricsComponent/LyricsComponent.js'
+import Player from './pages/Player/Player.js';
 
 var Spotify = require('node-spotify-api');
 
@@ -65,14 +63,7 @@ class App extends Component {
           <AlbumContainerComponent
             results={this.state.searchResults}
           />
-          <PlayButtonComponent/>
-          <SongImageComponent
-            src="/assets/images/1-Shine-On-You-Crazy-Diamond-pts1-5.jpg"
-            alt="1-Shine on You Crazy Diamond"
-            />
-          <LyricsComponent
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-            />
+          <Player />
         </header>
       </div>
     );
